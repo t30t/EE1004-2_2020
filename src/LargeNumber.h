148d30f3f5158d7e2794d887777ad3c18bb46c71
t30t <71395346+t30t@users.noreply.github.com>
2021-10-29 12:14:21 +0800

    Update LargeNumber.h

--- src/LargeNumber.h
+++ src/LargeNumber.h
@@ -20,46 +20,46 @@ using std::log10;
 class LargeNumber
 {
 public:
-	// null constructor, equivalent to LargeNumber(0)
-	LargeNumber();
-	// LargeNumber a("123456")
-	explicit LargeNumber(const string &raw);
-	// LargeNumber a(1)
-	LargeNumber(int raw);  // declared non-explicit as intended
+    // null constructor, equivalent to LargeNumber(0)
+    LargeNumber();
+    // LargeNumber a("123456")
+    explicit LargeNumber(const string &raw);
+    // LargeNumber a(1)
+    LargeNumber(int raw);  // declared non-explicit as intended
 
-	// copy constructor
-	LargeNumber(const LargeNumber &ref);
-	LargeNumber& operator=(const LargeNumber&);
+    // copy constructor
+    LargeNumber(const LargeNumber &ref);
+    LargeNumber& operator=(const LargeNumber&);
 
-	~LargeNumber();
+    ~LargeNumber();
 
-	LargeNumber operator+(const LargeNumber&) const;
-  	LargeNumber operator-(const LargeNumber&) const;
-	LargeNumber operator*(const LargeNumber&) const;
-	LargeNumber operator/(const LargeNumber&) const;
-	LargeNumber operator%(const LargeNumber&) const;
+    LargeNumber operator+(const LargeNumber&) const;
+    LargeNumber operator-(const LargeNumber&) const;
+    LargeNumber operator*(const LargeNumber&) const;
+    LargeNumber operator/(const LargeNumber&) const;
+    LargeNumber operator%(const LargeNumber&) const;
 
-	LargeNumber& trim();
+    LargeNumber& trim();
 
-	operator string() const;
+    operator string() const;
 
-	friend ostream& operator<<(ostream&, const LargeNumber&);
-	friend istream& operator>>(istream&, LargeNumber&);
+    friend ostream& operator<<(ostream&, const LargeNumber&);
+    friend istream& operator>>(istream&, LargeNumber&);
 
 private:
-	bool m_negative;
-	unsigned char* m_digits;
-	int m_size;
+    bool m_negative;
+    unsigned char* m_digits;
+    int m_size;
 
-	// second integer is just a placeholder
-	LargeNumber(int size, int);
+    // second integer is just a placeholder
+    LargeNumber(int size, int);
 
-	int getDigit(int index) const;
-	LargeNumber& setDigit(int index, int value);
+    int getDigit(int index) const;
+    LargeNumber& setDigit(int index, int value);
 
-	LargeNumber& lshift1();
-	LargeNumber negative() const;
-	LargeNumber abs() const;
+    LargeNumber& lshift1();
+    LargeNumber negative() const;
+    LargeNumber abs() const;
 
-	bool abs_larger_than(const LargeNumber&) const;
+    bool abs_larger_than(const LargeNumber&) const;
 };
